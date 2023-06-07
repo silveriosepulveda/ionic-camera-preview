279add9a7e0a2eb8e8334e880bdf2862eece9904
Silvério <silveriosepulveda@yahoo.com.br>
2023-06-07 08:44:30 -0300

    Funcionando

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,37 @@
-import { useState } from 'react';
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from "react";
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 
-import { CameraPreview, CameraPreviewOptions } from '@ionic-native/camera-preview';
-import { AndroidPermissions } from '@ionic-native/android-permissions'
+import {
+  CameraPreview,
+  CameraPreviewOptions,
+} from "@capacitor-community/camera-preview";
+
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem'
 
 const Home: React.FC = () => {
-  const [status, setStatus] = useState('Aguardando');
-  const [tempo, setTempo] = useState(0);
+  const [status, setStatus] = useState("Aguardando");
+  const [tempo, setTempo] = useState(0);  
+  const [url, setUrl] = useState('');
 
   const contador = (tempo: number) => {
     setTimeout(() => {
-      const novoTempo: number = tempo + 1;
-      setTempo(novoTempo)
-      if (status === 'Gravando')
-        contador(novoTempo)
-    }, 1000);
-  }
+      const novoTempo: number = tempo - 1;
+      console.log('****************    CAMERA APP CONTADOR     ', novoTempo);
 
+      setTempo(novoTempo)
+      if (novoTempo > 0)
+        contador(novoTempo)
+      else
+        parar();
+    }, 1000);
+  };
 
   //@ionic native preview
   const abrirCamera = () => {
@@ -25,74 +40,67 @@ const Home: React.FC = () => {
       y: 112,
       width: 300,
       height: 400,
-      camera: CameraPreview.CAMERA_DIRECTION.BACK,
-    }
+      storeToFile: false
+    };
 
-    CameraPreview.startCamera(opcoes).then(sucesso => {
-      iniciar();
-    }).catch(erro => alert(erro))
-    setStatus('Iniciando');
-  }
-
+    CameraPreview.start(opcoes)
+      .then((sucesso) => {
+        console.log("***********  CAMERA APP  *********** START **** ", sucesso);
+      })
+      .catch((erro) =>
+        console.log("*********** CAMERA APP ********* startCamera catch", erro)
+      );
+    setStatus("Iniciando");
+  };
 
   const fecharCamera = () => {
-    CameraPreview.stopCamera();
-  }
+    CameraPreview.stop();
+  };
 
-  const gravar = (video: any) => {
-    alert('deu')
-    alert(video)
-  }
+  const iniciar = async () => {
+    setStatus("Gravando");
 
-  const erro = (erro: any) => {
-    alert(erro)
-  }
-
-  const iniciar = () => {
-
-    AndroidPermissions.checkPermission(AndroidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE).then(success => {
-      if (!success.hasPermission) {
-        AndroidPermissions.requestPermission(AndroidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE).then(request => {
-          alert(JSON.stringify(request))
-          if (request.hasPermission) {            
-            contador(0)
-            setStatus('Gravando');
-            const opcoes = {
-              cameraDirection: CameraPreview.CAMERA_DIRECTION.BACK,
-              quality: 60,
-            }
-            CameraPreview.startRecordVideo(opcoes).then(filePath => {
-              alert(filePath)
-            }).catch(error => {
-              alert(error)
-            })
-          }
-
-        }).catch(errorReq => {
-          alert(JSON.stringify(errorReq))
-        })
-      }
-      else {
-        alert(JSON.stringify(success))
-      }
-
-    }).catch(error => {
-      alert(erro)
-    })
-
-  }
+    await (CameraPreview as any)
+      .startRecordVideo(null)
+      .then(async (filePath: any) => {        
+        contador(15);
+        console.log(
+          "*********** CAMERA APP ********* startRecordVideo then",
+          filePath
+        );
+      })
+      .catch((error: any) => {
+        console.log(
+          "*********** CAMERA APP ********* startRecordVideo catch",
+          error
+        );
+      });
+  };
 
   const parar = () => {
-    setStatus('finalizando')
+    setStatus("finalizando");
 
-    CameraPreview.stopRecordVideo().then(filePath => {
-      alert(filePath)
-    }).catch(error => {
-      alert(error)
-      fecharCamera();
-    })
-  }
+    (CameraPreview as any)
+      .stopRecordVideo()
+      .then(async (filePath: any) => {
+        const camVideo = 'file://' + filePath.videoFilePath;
 
+        const video = await Filesystem.readFile({ path: camVideo })
+        // const binaryString = atob(video.data);
+        // const blob = new Blob([binaryString], { type: "video/mp4" });
+        setUrl(video.data);
+
+        console.log("*********** CAMERA APP *********    STOPING REC     ", video.data);
+        fecharCamera();
+      })
+      .catch((error: any) => {
+        console.log(
+          "*********** CAMERA APP *********  stopRecordVideo ",
+          error
+        );
+        //fecharCamera();
+      });
+  };
 
   return (
     <IonPage>
@@ -101,19 +109,32 @@ const Home: React.FC = () => {
           <IonTitle>Captura de Vídeos</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent >
-        <h3> {status} -- {tempo} </h3>
+      <IonContent>
+        <h3>
+          {" "}
+          {status} -- {tempo}{" "}
+        </h3>
         <hr />
         <div style={{ marginTop: 400 }}></div>
 
-
-        <IonButton onClick={abrirCamera} className="btn btn-primary">Abrir </IonButton>
-        <IonButton onClick={iniciar} className="btn btn-primary">Iniciar </IonButton>
-        <IonButton onClick={parar} className="btn btn-primary">Parar</IonButton>
+        <IonButton onClick={abrirCamera} className="btn btn-primary">
+          Abrir{" "}
+        </IonButton>
+        <IonButton onClick={iniciar} className="btn btn-primary">
+          Iniciar{" "}
+        </IonButton>
+        <IonButton onClick={parar} className="btn btn-primary">
+          Parar
+        </IonButton>
       </IonContent>
+      <br /><br /><br /><br />
+      {url != '' &&
+        <video width={300} height={300} controls={true}>
+          <source src={"data:video/mp4;base64," + url} type="video/mp4" />
+        </video>
+      }
     </IonPage>
   );
-
 };
 
 export default Home;
